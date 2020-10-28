b2a34a467ebf86e30d6a902e6095df1631780442
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-10-28 15:20:26 +0530

    Added code for validating user first name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
-echo "Welcome to User Registration Validation Problem"
+while [[ True ]]
+do
+    echo "Choose any of the options below for validating your details:"
+    echo "To enter first name select option 1 "
+    echo "To exit select option 6"
+    read -p "enter your option:" option
+
+    case $option in
+        1)
+            #first name should start with capital letter and
+            #has minimum 3 characters
+            pattern_check="^[A-Z][a-zA-Z]{2,}$"
+            ;;
+        5)
+            echo "exiting......"
+            exit
+            ;;
+        *)
+            echo 'Invalid entry.enter valid option'
+            ;;
+    esac
+
+    if [[ $option =~ ^[1]$ ]]
+    then
+        read -p "enter your input:" pattern_to_be_checked
+        if [[ $pattern_to_be_checked =~ $pattern_check ]]
+        then
+            echo "$pattern_to_be_checked is valid"
+        else
+            echo "$pattern_to_be_checked is not valid"
+        fi
+    fi
+
+done 
