b360babb1f33d86fdb0d5784d0bb71757a76da11
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-10-28 15:27:46 +0530

    Added code for validating user email

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -4,6 +4,7 @@ do
     echo "Choose any of the options below for validating your details:"
     echo "To enter first name select option 1 "
     echo "To enter last name select option 2"
+    echo "To enter email select option 3"
     echo "To exit select option 6"
     read -p "enter your option:" option
 
@@ -12,6 +13,12 @@ do
             #it should start with capital letter and
             #has minimum 3 characters
             pattern_check="^[A-Z][a-zA-Z]{2,}$"
+            ;;
+        3)
+            #abc.xyz@bl.co.in
+            #email has 3 mandatory parts(abc,bl&co) and 2 optional
+            #(xyz & in ) with precise @ and . postions
+            pattern_check="^[a-z]{3,}(.[a-z]*)?@([a-z]){2,}.[a-z]+(.in)*"
             ;;
         5)
             echo "exiting......"
@@ -22,7 +29,7 @@ do
             ;;
     esac
 
-    if [[ $option =~ ^[12]$ ]]
+    if [[ $option =~ ^[123]$ ]]
     then
         read -p "enter your input:" pattern_to_be_checked
         if [[ $pattern_to_be_checked =~ $pattern_check ]]
@@ -33,4 +40,4 @@ do
         fi
     fi
 
-done
+done 
